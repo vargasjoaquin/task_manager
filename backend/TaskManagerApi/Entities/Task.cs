abaf2bffc04a3df9f5cdeec9d31aa1f193c5e332
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-04-21 18:37:03 -0300

    se crean las entidades de usuario, tarea y estado de la tarea

---
+++ backend/TaskManagerApi/Entities/Task.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagerApi.Entities
+{
+    [Table("Tasks")]
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion {  get; set; }
+        [Required]
+        public int IdEstado { get; set; }
+        [Required]
+        public int IdUsuario { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        [ForeignKey("IdEstado")]
+        public virtual Task EstadoTarea { get; set; } = null!;
+
+        [ForeignKey("IdUsuario")]
+        public virtual User Usuario { get; set; } = null!;
+    }
+}
---
+++ backend/TaskManagerApi/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagerApi.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
