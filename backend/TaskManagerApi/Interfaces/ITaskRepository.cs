86cb39b479650b8e968e7dc8901cf1de476431b7
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-04-21 19:34:54 -0300

    se agregan los dtos de entrada y salida referente a la tarea y se crean las interfaces para el servicio y el repositorio

---
+++ backend/TaskManagerApi/Interfaces/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using TaskManagerApi.Entities;
+using myTask = TaskManagerApi.Entities.Task;
+
+namespace TaskManagerApi.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<myTask>> GetAllAsync(int? idEstado); 
+        Task<myTask?> GetByIdAsync(int id);
+        Task AddAsync(myTask tarea);
+        Task UpdateAsync(myTask tarea);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
---
+++ backend/TaskManagerApi/Interfaces/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using TaskManagerApi.DTOs.Requests;
+using TaskManagerApi.DTOs.Responses;
+
+namespace TaskManagerApi.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<TaskResponseDto>> GetAllTasksAsync(int? idEstado);
+        Task<TaskResponseDto?> GetTaskByIdAsync(int id);
+        Task<TaskResponseDto> CreateTaskAsync(TaskRequestDto taskDto);
+        Task<bool> UpdateTaskAsync(int id, TaskRequestDto taskDto);
+        Task<bool> DeleteTaskAsync(int id);
+    }
+}
